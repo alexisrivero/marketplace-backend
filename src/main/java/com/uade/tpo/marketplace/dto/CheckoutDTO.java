950b7c5ab05a52db512fb42b3f7bc558f88aefb5
alexisrivero <alexisr15494@gmail.com>
2024-09-04 14:16:42 -0300

    added needed DTOs

---
+++ src/main/java/com/uade/tpo/marketplace/dto/CheckoutDTO.java
@@ -0,0 +1,24 @@
+package com.uade.tpo.marketplace.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CheckoutDTO {
+//DTO to show on JSON all the info of a checkout
+
+    private String userName;
+    private String firstName;
+    private String lastName;
+    private List<CheckoutProductDTO> checkoutProducts;
+    private CreateAddressDTO address;
+    private PaymentMethodNoIdDTO paymentMethod;
+    private double subTotal;
+}
